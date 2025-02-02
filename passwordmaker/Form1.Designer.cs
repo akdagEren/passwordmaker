64a5989dca8b263f43205ec2c6e14a7132c8e725
Eren <Eren@DESKTOP-79PHVRI>
2025-02-03 00:57:24 +0300

    Proje dosyası ekle.

---
+++ passwordmaker/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace passwordmaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numUzunluk = new System.Windows.Forms.NumericUpDown();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.btnKopyala = new System.Windows.Forms.Button();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numUzunluk)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numUzunluk
+            // 
+            this.numUzunluk.Location = new System.Drawing.Point(326, 75);
+            this.numUzunluk.Name = "numUzunluk";
+            this.numUzunluk.Size = new System.Drawing.Size(120, 22);
+            this.numUzunluk.TabIndex = 0;
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(91, 180);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(75, 23);
+            this.btnOlustur.TabIndex = 1;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // btnKopyala
+            // 
+            this.btnKopyala.Location = new System.Drawing.Point(591, 180);
+            this.btnKopyala.Name = "btnKopyala";
+            this.btnKopyala.Size = new System.Drawing.Size(75, 23);
+            this.btnKopyala.TabIndex = 2;
+            this.btnKopyala.Text = "Kopyala";
+            this.btnKopyala.UseVisualStyleBackColor = true;
+            this.btnKopyala.Click += new System.EventHandler(this.btnKopyala_Click);
+            // 
+            // txtParola
+            // 
+            this.txtParola.Location = new System.Drawing.Point(237, 180);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.Size = new System.Drawing.Size(320, 22);
+            this.txtParola.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.btnKopyala);
+            this.Controls.Add(this.btnOlustur);
+            this.Controls.Add(this.numUzunluk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numUzunluk)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numUzunluk;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.Button btnKopyala;
+        private System.Windows.Forms.TextBox txtParola;
+    }
+}
+
---
+++ passwordmaker/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordmaker
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string karakterler = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int uzunluk = (int)numUzunluk.Value;
+                txtParola.Text = ParolaOlustur(uzunluk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private string ParolaOlustur(int uzunluk)
+        {
+            StringBuilder parola = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < uzunluk; i++)
+            {
+                parola.Append(karakterler[rnd.Next(karakterler.Length)]);
+            }
+
+            return parola.ToString();
+        }
+
+        private void btnKopyala_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtParola.Text))
+            {
+                Clipboard.SetText(txtParola.Text);
+                MessageBox.Show("Parola panoya kopyalandı!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Önce bir parola oluşturun!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+
+
+}
